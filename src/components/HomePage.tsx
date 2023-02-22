cdeef99b8a07a2c0edfeff6e9822d17eb6d63765
Keadeish <you@example.com>
2023-02-22 15:22:08 +0000

    removed ref

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 import { IUserData } from "../utils/animeStructure";
 export function HomePage(): JSX.Element {
